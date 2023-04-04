495b7b833c82a2eb8f90b3daa76921ddd6a827c2
ihossain-6 <ismumhossain@gmail.com>
2023-04-04 14:24:51 +0600

    Update

--- tests/unit/test_crowdfund.py
+++ tests/unit/test_crowdfund.py
@@ -1,25 +1,16 @@
-from scripts.deploy_crowdfund import deploy_crowdfund
-from scripts.helper_functions import get_account
 from web3 import Web3
-import pytest
 
-def test_initialization():
-    account = get_account
-    crowdfund = deploy_crowdfund
+def test_initialization(crowdfund, owner):
     deadline = 1683009675
-    assert crowdfund.get_owner() == account
+    assert crowdfund.get_owner() == owner
     assert crowdfund.get_deadline() == deadline
     
-def test_fund():
-    account = get_account
+def test_fund(crowdfund, funder):
     value = Web3.to_wei(1,"ether")
-    crowdfund = deploy_crowdfund
-    crowdfund.fund(sender = account, value = value)
-    assert crowdfund.get_funders(account) == value
+    crowdfund.fund(sender = funder, value = value)
+    assert crowdfund.get_funders(funder) == value
     assert crowdfund.get_amount() == value
     
-def test_withdraw():
-    account = get_account
-    crowdfund = deploy_crowdfund
-    crowdfund.withdraw(sender = account)
+def test_withdraw(crowdfund, owner):    
+    crowdfund.withdraw(sender = owner)
     assert crowdfund.get_amount() == 0
